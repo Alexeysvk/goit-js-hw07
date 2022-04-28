8a47117cbea611682b6b9766a22d881df454dcab
Alexeysvk <83663482+Alexeysvk@users.noreply.github.com>
2022-04-28 20:44:35 +0400

    ок

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+(function createMarkup (){
+    const itemMarkup= galleryItems.map(({ original, preview, description })=>{
+    return `
+    <a class="gallery__item" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+    }).join("");
+    gallery.insertAdjacentHTML('beforeend', itemMarkup)
+}());
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionsType: "alt",
+    captionDelay: 250,
+});
 console.log(galleryItems);
